9da46aa134c7b2af7045e9e13e2daa5d0cfb8c47
SevenGriphon <152780844+SeveneGriphon@users.noreply.github.com>
2025-10-05 23:05:13 +0200

    Add button to resize the grid

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
+const startGridSize = 16
 const container = document.querySelector(".container")
+
+function generateGrid(size) {
+    document.querySelectorAll(".container div").forEach((div) => {
+        div.remove()
+    })
+
+    for (let y = 0; y < size; y++) {
+        const row = document.createElement("div")
+        row.classList.add("row")
+        for (let i = 0; i < size; i++) {
+            const div = document.createElement("div")
+            div.addEventListener("mouseenter", onHover)
+            row.appendChild(div)
+        }
+        container.appendChild(row)
+    }
+}
+
+function changeGridSize(e) {
+    let size = Math.min(prompt("Enter the grid size"), 100)
+    
+    generateGrid(size)
+}
 
 function onHover(e) {
     e.target.classList.add("on")
 }
 
-for (let y = 0; y < 16; y++) {
-    const row = document.createElement("div")
-    row.classList.add("row")
-    for (let i = 0; i < 16; i++) {
-        const div = document.createElement("div")
-        div.addEventListener("mouseenter", onHover)
-        row.appendChild(div)
-    }
-    container.appendChild(row)
-}
+generateGrid(startGridSize)
+
+const sizeBtn = document.querySelector(".grid-size-button")
+sizeBtn.addEventListener("click", changeGridSize)
