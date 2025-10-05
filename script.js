c6d4dd9302306ea4ee9b910c0ee421bdf82a2bfd
SevenGriphon <152780844+SeveneGriphon@users.noreply.github.com>
2025-10-05 22:33:49 +0200

    Add a "hover" effect to the grid

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const container = document.querySelector(".container")
 
+function onHover(e) {
+    e.target.classList.add("on")
+}
 
 for (let y = 0; y < 16; y++) {
     const row = document.createElement("div")
     row.classList.add("row")
     for (let i = 0; i < 16; i++) {
         const div = document.createElement("div")
+        div.addEventListener("mouseenter", onHover)
         row.appendChild(div)
     }
     container.appendChild(row)
